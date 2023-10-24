e14aecd041bc7c7df2af258f99932cf5cd7f3d1d
phuongNV <nvp.1312.2001@gmail.com>
2023-10-24 07:38:05 +0700

    121212

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -2,7 +2,11 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SearchIcon() {
-    return <FontAwesomeIcon icon={faMagnifyingGlass} />;
+    return (
+        <div>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />;<h2>abc</h2>
+        </div>
+    );
 }
 
 export default SearchIcon;
