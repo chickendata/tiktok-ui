7d605c3c88a87d245096e4f0c543232bda267138
phuongNV <nvp.1312.2001@gmail.com>
2023-10-24 07:54:16 +0700

    BaiXuLiNhieuFileIndex

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,12 +1,1 @@
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-function SearchIcon() {
-    return (
-        <div>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />;<h2>abc</h2>
-        </div>
-    );
-}
-
-export default SearchIcon;
+export * from './Icons';
